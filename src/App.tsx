289e57a9f6a53d1107784a6cd62ec1fe38e9c856
Cyberady <adityagdg07@gmail.com>
2026-04-06 19:38:49 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Tailwind Working</h1>
+    </div>
+  );
+}
